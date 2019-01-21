e72d192442273236fa2bf7716583e6e672697d2d
Iza <jza.q@wp.pl>
2019-01-21 19:34:33 +0100

    Models were created

--- nzoz/admin.py
+++ nzoz/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Clinic
+from .models import Specialty
+from .models import Day
+from .models import Specialist
+from .models import Schedule
 
-# Register your models here.
+admin.site.register(Clinic)
+admin.site.register(Specialty)
+admin.site.register(Day)
+admin.site.register(Specialist)
+admin.site.register(Schedule)
+
+
--- nzoz/models.py
+++ nzoz/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Clinic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Day(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Specialist(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Schedule(models.Model):
+    hourFrom = models.TimeField()
+    hourTo = models.TimeField()
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+
+    
+
+
